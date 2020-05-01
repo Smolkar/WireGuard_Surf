878710f64f65b8e4a5c86d90690c595429d4ef95
dimopopov <dimo.popov97@gmail.com>
2020-05-01 19:07:02 +0200

    Init commit

---
+++ Configuration.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	log "github.com/labstack/gommon/log"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type WgConf struct {
+	configPath string
+	PrivateKey string
+	PublicKey  string
+	Users      map[string]*UserConf
+}
+type UserConf struct {
+	Name    string
+	Clients map[string]*ClientConfig
+}
+type ClientConfig struct {
+	Name       string
+	PrivateKey string
+	PublicKey  string
+	IP         net.IP
+	Created    string
+	Modified   string
+	Info       string
+}
+
+func newServerConfig(cfgPath string) *WgConf {
+	keys, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		log.Error("Fatal", err)
+	}
+	config := &WgConf{
+		configPath: cfgPath,
+		PrivateKey: keys.String(),
+		PublicKey:  keys.PublicKey().String(),
+		Users:      make(map[string]*UserConf),
+	}
+	file, err := os.Open(filepath.Clean(cfgPath))
+	if err == nil {
+		if err = json.NewDecoder(file).Decode(config); err != nil {
+			log.Fatal("Failing to decode ", err)
+		}
+		log.Info("Read server config from file: ", cfgPath)
+	} else if os.IsNotExist(err) {
+		log.Info("No configuration file found: ", cfgPath)
+		err = config.Write()
+
+	}
+	log.Print("PublicKey: ", config.PublicKey, "PrivateKey", config.PrivateKey, )
+	if err != nil {
+		log.Info("Error", err)
+	}
+	return config
+}
+func newClientConfig(ip net.IP, Name, Info string) *ClientConfig {
+	keys, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := ClientConfig{
+		Name:       Name,
+		PrivateKey: keys.String(),
+		PublicKey:  keys.PublicKey().String(),
+		IP:         ip,
+		Created:    time.Now().Format(time.RFC3339),
+		Modified:   time.Now().Format(time.RFC3339),
+		Info:       Info,
+	}
+	return &config
+}
+func (config *WgConf) Write() error {
+	data, err := json.MarshalIndent(config, "", " ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(config.configPath, data, 0600)
+}
+
+func (config *WgConf) GetUSerConfig(user string) *UserConf {
+	us, ok := config.Users[user]
+	if !ok {
+		log.Info("This user is not existing: ", user, " Making one righ now.....")
+		us = &UserConf{
+			Name:    user,
+			Clients: make(map[string]*ClientConfig),
+		}
+		config.Users[user] = us
+	}
+	return us
+}
+
+func NewClientConfig(ip net.IP, Name, Info string) *ClientConfig {
+	keys, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		log.Fatal("Failed to generate keys: ", err)
+	}
+	config := ClientConfig{
+		Name:       Name,
+		PrivateKey: keys.String(),
+		PublicKey:  keys.PublicKey().String(),
+		IP:         ip,
+		Created:    time.Now().Format(time.RFC3339),
+		Modified:   time.Now().Format(time.RFC3339),
+		Info:       Info,
+	}
+	return &config
+}
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/labstack/gommon/log"
+	"github.com/vishvananda/netlink"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"sync"
+)
+
+var (
+	dataDir = kingpin.Flag("data-dir", "Directory used for storage").Default("/Config/lib").String()
+	//listenAddr            = kingpin.Flag("listen-address", "Address to listen to").Default(":8080").String()
+	//natEnabled            = kingpin.Flag("nat", "Whether NAT is enabled or not").Default("true").Bool()
+	//natLink               = kingpin.Flag("nat-device", "Network interface to masquerade").Default("wlp2s0").String()
+	clientIPRange         = kingpin.Flag("client-ip-range", "Client IP CIDR").Default("172.31.255.0/24").String()
+	//authUserHeader        = kingpin.Flag("auth-user-header", "Header containing username").Default("X-Forwarded-User").String()
+	//maxNumberClientConfig = kingpin.Flag("max-number-client-config", "Max number of configs an client can use. 0 is unlimited").Default("0").Int()
+	//
+	wgLinkName   = kingpin.Flag("wg-device-name", "WireGuard network device name").Default("wg0").String()
+	//wgListenPort = kingpin.Flag("wg-listen-port", "WireGuard UDP port to listen to").Default("51820").Int()
+	//wgEndpoint   = kingpin.Flag("wg-endpoint", "WireGuard endpoint address").Default("127.0.0.1:51820").String()
+	//wgAllowedIPs = kingpin.Flag("wg-allowed-ips", "WireGuard client allowed ips").Default("0.0.0.0/0").Strings()
+	//wgDNS        = kingpin.Flag("wg-dns", "WireGuard client DNS server (optional)").Default("").String()
+	tlsCertDir = "."
+	tlsKeyDir  = "."
+
+
+
+)
+type Server struct{
+	serverConfPath string
+	mutex sync.RWMutex
+	Config *WgConf
+	IPAddr net.IP
+	clientIPRange *net.IPNet
+	assets http.Handler
+}
+
+type wgLink struct{
+	attrs *netlink.LinkAttrs
+}
+
+
+func (w *wgLink) Attrs() *netlink.LinkAttrs {
+	return w.attrs
+}
+
+func (w *wgLink) Type() string {
+	return "wireguard"
+}
+
+func NewServer() *Server {
+	ipAddr, ipNet, err := net.ParseCIDR("10.0.0.0/8")
+	if err != nil{
+		log.Fatal("Error with those IPS:",err)
+	}
+	log.Info("IP Address: %s IP Network: %s", ipAddr, ipNet)
+	err = os.Mkdir(*dataDir,0700)
+	if err != nil{
+		log.Debug(	"Error init dir: ", err)
+	}
+	configPath := path.Join(*dataDir, "conf")
+	log.Debug(configPath, )
+	config := newServerConfig(configPath)
+
+	assets := http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: ""})
+	surf := Server{
+		serverConfPath: configPath,
+		mutex:          sync.RWMutex{},
+		Config:         config,
+		IPAddr:         ipAddr,
+		clientIPRange:  ipNet,
+		assets:         assets,
+	}
+	fmt.Println(assets)
+	return &surf
+}
+
+func (serv *Server) UpInterface() error{
+	attrs := netlink.NewLinkAttrs()
+	attrs.Name = *wgLinkName
+	link := wgLink{attrs: &attrs}
+	log.Debug("Adding WireGuard device %s", *wgLinkName)
+	err := netlink.LinkAdd(&link)
+	if os.IsExist(err){
+		log.Info("WireGuard interface %s already exists. REUSING. ", *wgLinkName)
+	} else if err != nil{
+		return err
+	}
+	log.Debug("Setting up IP address to wireguard device: ", )
+	addr, _ := netlink.ParseAddr(*clientIPRange)
+	err = netlink.AddrAdd(&link, addr)
+	return nil
+}
+
+func main(){
+
+	s := NewServer()
+	s.UpInterface()
+}
+
+func getTlsConfig() *tls.Config {
+	caCertFile := filepath.Join(tlsCertDir, "ca.crt")
+	certFile := filepath.Join(tlsCertDir, "server.crt")
+	keyFile := filepath.Join(tlsKeyDir, "server.key")
+
+	keyPair, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCertPem, err := ioutil.ReadFile(caCertFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	trustedCaPool := x509.NewCertPool()
+	if !trustedCaPool.AppendCertsFromPEM(caCertPem) {
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{keyPair},
+		MinVersion:   tls.VersionTLS12,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    trustedCaPool,
+		CipherSuites: []uint16{tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384},
+	}
+}
